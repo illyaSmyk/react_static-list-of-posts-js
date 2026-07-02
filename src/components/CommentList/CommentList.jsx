9c48c5f6b324ec4ddc7a4a4402b07679f812f6f1
ILLYA SMYK <smykillya@gmail.com>
2026-07-02 16:46:54 +0200

    add task solution3

--- src/components/CommentList/CommentList.jsx
+++ src/components/CommentList/CommentList.jsx
@@ -1,13 +1,9 @@
-import { Fragment } from 'react';
-
 import { CommentInfo } from '../CommentInfo/CommentInfo';
 
 export const CommentList = ({ comments }) => (
   <div className="CommentList">
     {comments.map(comment => (
-      <Fragment key={comment.id}>
-        <CommentInfo comment={comment} />
-      </Fragment>
+      <CommentInfo key={comment.id} comment={comment} />
     ))}
   </div>
 );
